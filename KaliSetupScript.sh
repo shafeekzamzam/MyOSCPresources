efbac67844461f1dcc8fab0a390945af5488e7a4
Shafeek <shafeekzamzam@gmail.com>
2021-01-28 13:55:59 -0500

    initial commit

--- KaliSetupScript.sh
+++ KaliSetupScript.sh
@@ -18,13 +18,25 @@ sudo apt install code
 Github Setup & Config Commands
 ============================
 git config --global user.email "shafeekzamzam@gmail.com";git config --global user.name "Shafeek"
-git push
-git commit
+
+git commit;git push
 =======================
 Edit ZSHRC for Box Automation
 =======================
 
+
+
+
+=======
+Vulscan DB
+=========
+git clone https://github.com/scipag/vulscan scipag_vulscan
+sudo ln -s `pwd`/scipag_vulscan /usr/share/nmap/scripts/vulscan    
+
+eg:nmap -sV --script=vulscan/vulscan.nse www.example.com
+
+============
+NMAP
+============
 nmap -sC -sV -p0-p1000 $1 > result1.txt &
-
-nmap script vulscan
-
+nmap -sV --script=vulscan/vulscan.nse $1 > result2.txt &
